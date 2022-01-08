ca80e6698df985a71861ec5e6914e095198f0abd
Gabriel Rendon Paredes <gabriel.rendon@Gabriels-MacBook-Pro.local>
2022-01-07 22:06:49 -0600

    Refactor Enviroment init

--- enviroment.rb
+++ enviroment.rb
@@ -3,17 +3,15 @@ module Enviroment
     attr_accessor :packs
 
     def initialize
-      @packs = {}
-      @packs[:simple_transportation_pack] = { intelligence: [:cellphone], items: %i[medipack chevy_versa] }
-      @packs[:standard_transportation_pack] =
-        { intelligence: %i[cellphone antenna], arsenal: [:colt_1911],
-          items: %i[handcuffs medipack chemestry chevy_versa] }
-      @packs[:simple_mission_pack] =
-        { intelligence: %i[infopack laptop cellphone antenna], arsenal: [:colt_1911],
-          items: %i[handcuffs medipack chemistry financial] }
-      @packs[:standard_mission_pack] =
-        { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[remington_870 colt_1911 machete hatchet],
-          items: %i[handcuffs medipack chemistry financial] }
+      @packs = {
+        simple_transportation_pack: { intelligence: [:cellphone], items: %i[medipack chevy_versa] },
+        standard_transportation_pack: { intelligence: %i[cellphone antenna], arsenal: [:colt_1911],
+                                        items: %i[handcuffs medipack chemestry chevy_versa] },
+        simple_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: [:colt_1911],
+                               items: %i[handcuffs medipack chemistry financial] },
+        standard_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[remington_870 colt_1911 machete hatchet],
+                                 items: %i[handcuffs medipack chemistry financial] }
+      }
     end
   end
 
@@ -21,10 +19,12 @@ module Enviroment
     attr_accessor :missions
 
     def initialize
-      @missions
+      @missions = {}
     end
 
-    def new_mission(name:, objective:, pack:); end
+    def new_mission(name:, objective:, pack:)
+      missions[name] = { objective: objective, status: :active, pack: pack }
+    end
   end
 
   class Human
