ab9049a4471c578412a15d1ba76dbba9edf52d32
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-01-09 10:35:00 -0600

    Add game testing

--- game.rb
+++ game.rb
@@ -14,13 +14,27 @@ class Game
     }
   end
 
-  def new_worker(name)
+  def new_worker(name:)
     worker = Enviroment::Worker.new(name: name)
     board[:players].store(name, worker)
   end
+
+  def new_mission(name:, objective:, pack:)
+    control.new_mission(name: name, objective: objective, pack: pack)
+  end
 end
 
-g = Game.new
-g.new_worker 'test'
-g.new_worker 'another test'
-p g.board[:players]['test']
+game = Game.new
+
+diego = game.new_worker name: 'Diego'
+diego.set_personal_data surname: 'Mota', age: 40, marital_status: :single, children: 0, country: :mx, language: :es
+diego.set_professional_data position: 'SE', occupation: 'IT', skills: %i[ruby blender], observations: 'none'
+puts "#{diego.name}, #{diego.class}", diego.personal_data, diego.professional_data
+
+name = :alpha
+objective = 'Get Alpha to the base'
+pack = :simple_transportation_pack
+game.new_mission name: name, objective: objective, pack: game.depot.packs[pack]
+puts game.control.missions[:alpha]
+game.control.set_mission_to_accomplished(:alpha)
+puts game.control.missions[:alpha]
