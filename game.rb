5cd1d95314e9030f368f5cdafccb742cb1720cd7
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-01-09 09:32:36 -0600

    Add new worker method

--- game.rb
+++ game.rb
@@ -1,13 +1,26 @@
 require_relative 'enviroment'
 class Game
-  attr_accessor :depot, :control
+  attr_accessor :depot, :control, :board
 
   include Enviroment
 
   def initialize
     @depot = Enviroment::Depot.new
     @control = Enviroment::Control.new
-    @human = Enviroment::Human
-    @worker = Enviroment::Worker
+    @board = {
+      players: {},
+      control: {},
+      status: nil
+    }
+  end
+
+  def new_worker(name)
+    worker = Enviroment::Worker.new(name: name)
+    board[:players].store(name, worker)
   end
 end
+
+g = Game.new
+g.new_worker 'test'
+g.new_worker 'another test'
+p g.board[:players]['test']
