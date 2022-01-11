4aab25739f89827e3e14a60cecccdcb5db795b85
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-11 13:00:39 -0600

    Update error_test.rb

---
+++ error_test.rb
@@ -0,0 +1,27 @@
+require_relative "environment.rb"
+
+class ErrorTest
+  include Environment
+  attr_accessor :board, :depot, :control, :dictionary, :poker
+
+  def initialize
+    @board = {
+      players: {},
+      control: {},
+      status: nil
+    }
+    @control = Environment::Control.new
+  end
+
+  def new_worker(name:)
+    begin
+      @board[:players][name.to_sym] = Environment::Worker.new(name)
+      p @board
+    rescue NoMethodError
+      puts "No name for worker given"
+    end
+  end
+end
+
+errorEnv = ErrorTest.new
+newWorker = errorEnv.new_worker(name: "Hector")
