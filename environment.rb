aec6ed7b9a7be23de129a9d1a9c6cca245325346
Francisco Ramirez <francisco.rv@wizeline.com>
2022-01-10 19:24:03 -0600

    Initial commit, create environment.rb and game.rb   - environment.rb     - Create module Environment     - Create classes Depot, Control and Human

---
+++ environment.rb
@@ -0,0 +1,28 @@
+# create a module named Environment 
+module Environment
+  #create on it three empty classes called Depot, Control and Human.
+  # Depot class
+  class Depot
+
+    def initialize
+      @packs 
+    end
+
+    
+    
+  end
+
+
+  # Control class
+  class Control
+    @missions
+
+  end
+  
+  
+  # Human class
+  class Human
+    
+  end  
+
+end
