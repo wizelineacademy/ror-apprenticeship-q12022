da12c7d367f9f00f25df6c784f3f7774f854ba0b
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-11 15:47:26 -0600

    Update game.rb

--- game.rb
+++ game.rb
@@ -28,15 +28,15 @@ class Game
 end
 
 # Testing
-game = Game.new
-diego = game.new_worker name: 'Diego'
-diego.set_personal_data surname: 'Mota', age: 40, marital_status: :single, children: 0, country: :mx, language: :es
-diego.set_professional_data position: 'SE', occupation: 'IT', skills: %i[ruby blender], observations: 'none'
+# game = Game.new
+# diego = game.new_worker name: 'Diego'
+# diego.set_personal_data surname: 'Mota', age: 40, marital_status: :single, children: 0, country: :mx, language: :es
+# diego.set_professional_data position: 'SE', occupation: 'IT', skills: %i[ruby blender], observations: 'none'
 # puts "#{diego.name}, #{diego.class}", diego.personal_data, diego.professional_data
 
-name = :alpha
-objective = 'Get Alpha to the base'
-game.new_mission name: name, objective: objective, pack: game.depot.packs[:simple_transportation_pack]
+# name = :alpha
+# objective = 'Get Alpha to the base'
+# game.new_mission name: name, objective: objective, pack: game.depot.packs[:simple_transportation_pack]
 # puts game.control.missions[:alpha]
-game.control.set_mission_to_accomplished(:alpha)
+# game.control.set_mission_to_accomplished(:alpha)
 # puts game.control.missions[:alpha]
