be049a63cb7bb811ac42b37dc9e9b5d6fa5099f3
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-01-11 15:41:43 -0600

    Corrected RuboCop offenses

--- archiver.rb
+++ archiver.rb
@@ -1,32 +1,34 @@
-require "tempfile"
-require "fileutils"
-require "find"
+# frozen_string_literal: true
+
+require 'tempfile'
+require 'fileutils'
+require 'find'
 
 class Archiver
   attr_accessor :file
-  @@modes = [:reader, :writer, :both]
 
-  def initialize
-  end
+  @@modes = %i[reader writer both]
+
+  def initialize; end
 
   @@modes.each do |mode|
     define_method("open_file_as_#{mode}") do |file|
-        @file = File.open(file, "r") if mode == :reader
-        @file = File.open(file, "w") if mode == :writer
-        @file = File.open(file, "r+") if mode == :both
-      end
+      @file = File.open(file, 'r') if mode == :reader
+      @file = File.open(file, 'w') if mode == :writer
+      @file = File.open(file, 'r+') if mode == :both
     end
+  end
 
-    def read_file(lines: false)
-      lines ? data = self.file&.read : data = self.file&.readlines.map(&:chomp)
-    end
+  def read_file(lines: false)
+    data = lines ? file&.read : file&.readlines.map(&:chomp)
+  end
 
-    def write_file(data: nil, append: true)
-      append ? File.write(self.file, data, mode: "a") : File.write(self.file, data)
-    end
+  def write_file(data: nil, append: true)
+    append ? File.write(file, data, mode: 'a') : File.write(file, data)
+  end
 end
 
-#archiver = Archiver.new
-#archiver.open_file_as_both "lines.txt"
-#archiver.write_file data: "line 4", append: true
-#p archiver.read_file
+# archiver = Archiver.new
+# archiver.open_file_as_both "lines.txt"
+# archiver.write_file data: "line 4", append: true
+# p archiver.read_file
