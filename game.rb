0f78a2749d7ce272b8ce9ff2e4b95fd097a58e68
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-01-11 12:21:55 -0600

    Refactor some methods

--- game.rb
+++ game.rb
@@ -29,12 +29,12 @@ game = Game.new
 diego = game.new_worker name: 'Diego'
 diego.set_personal_data surname: 'Mota', age: 40, marital_status: :single, children: 0, country: :mx, language: :es
 diego.set_professional_data position: 'SE', occupation: 'IT', skills: %i[ruby blender], observations: 'none'
-puts "#{diego.name}, #{diego.class}", diego.personal_data, diego.professional_data
+# puts "#{diego.name}, #{diego.class}", diego.personal_data, diego.professional_data
 
 name = :alpha
 objective = 'Get Alpha to the base'
 pack = :simple_transportation_pack
 game.new_mission name: name, objective: objective, pack: game.depot.packs[pack]
-puts game.control.missions[:alpha]
+# puts game.control.missions[:alpha]
 game.control.set_mission_to_accomplished(:alpha)
-puts game.control.missions[:alpha]
+# puts game.control.missions[:alpha]
