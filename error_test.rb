d1e3df46ab26be8d46e7748898c8f1daba33b229
Lisset Cuevas Juarez <lisset.cuevas@Lissets-MacBook-Pro.local>
2022-01-12 11:03:08 -0600

    Error test class

---
+++ error_test.rb
@@ -0,0 +1,22 @@
+require_relative 'environment.rb'
+
+class ErrorTest
+  include Environment
+  attr_accessor :board, :depot, :control, :dictionary, :poker
+
+  def initialize
+    @board = { players: Hash.new, control: Hash.new, status: nil }
+    @control = Environment::Control.new
+  end
+
+  def new_worker name: nil
+    begin
+      @board[:players].store name, Environment::Worker.new(name)
+    rescue NoMethodError
+      puts 'No name for worker given'
+    end
+  end
+end
+
+# This error handling method validates that every worker has a named
+# set two more validation error handling methods.
