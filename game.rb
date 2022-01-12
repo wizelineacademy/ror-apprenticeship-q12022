1f2fb7906cc985bab469bd61f968f6b19a5740c6
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-01-11 18:53:13 -0600

    Add erroneous method calls

--- game.rb
+++ game.rb
@@ -24,17 +24,3 @@ class Game
   end
 end
 
-game = Game.new
-
-diego = game.new_worker name: 'Diego'
-diego.set_personal_data surname: 'Mota', age: 40, marital_status: :single, children: 0, country: :mx, language: :es
-diego.set_professional_data position: 'SE', occupation: 'IT', skills: %i[ruby blender], observations: 'none'
-# puts "#{diego.name}, #{diego.class}", diego.personal_data, diego.professional_data
-
-name = :alpha
-objective = 'Get Alpha to the base'
-pack = :simple_transportation_pack
-game.new_mission name: name, objective: objective, pack: game.depot.packs[pack]
-# puts game.control.missions[:alpha]
-game.control.set_mission_to_accomplished(:alpha)
-# puts game.control.missions[:alpha]
