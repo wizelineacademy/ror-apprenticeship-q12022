988fbe044fdeef031d008a9ac3de40fe84227c55
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-17 09:59:18 -0600

    Created archiver file. Added archiver class from implementation given.

---
+++ archiver.rb
@@ -0,0 +1,36 @@
+require 'tempfile'
+require 'fileutils'
+require 'find'
+
+class Archiver
+  attr_accessor :file
+  @@modes = [:reader, :writer, :both]
+
+  def initialize
+  end
+
+  @@modes.each do |mode|
+    define_method("open_file_as_#{mode}") do |file|
+      @file = File.open(file, 'r') if mode == :reader
+      @file = File.open(file, 'w') if mode == :writer
+      @file = File.open(file, 'r+') if mode == :both
+    end
+  end
+
+  def read_file lines: false
+    lines ? data = self.file&.read : data = self.file&.readlines.map(&:chomp)
+  end
+
+  def write_file data: nil, append: true
+    append ? File.write(self.file, data, mode: "a") : File.write(self.file, data)
+  end  
+end
+
+# Reading a file using tempfile
+file = Tempfile.new('foo')
+file.path      # => A unique filename in the OS's temp directory,
+file.write("hello world")
+file.rewind
+file.read      # => "hello world"
+file.close
+file.unlink    # deletes the temp file
