61f526cafbe92fa050ba81d564b07b62efad7ce5
Erick Godoy <erick.godoy@wizeline.com>
2022-01-17 16:58:28 -0700

    Add capital case to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'resolv-replace' #This solve net issue 'Net::OpenTimeout: Failed to open TCP connection'
+
 pokedex_json = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=150')
 
 pokedex_json["results"].each do |item|
   poke_request = HTTParty.get(item["url"])
   poke_new = Pokemon.new
   poke_new.name = poke_request["name"]
+  poke_new.name[0] = poke_new.name[0].capitalize
   poke_new.poke_id = poke_request["id"]
   poke_new.image_url = poke_request["sprites"]["front_default"]
-  poke_new.sort = poke_request["types"].map{|type| type["type"]["name"]}.join(",")
+  poke_new.sort = poke_request["types"].map{|type| type["type"]["name"].slice(0,1).capitalize + type["type"]["name"].slice(1..-1)}.join(",")
   poke_new.save
 end
