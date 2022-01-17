55d2e38b55b252e4d7bc6e576a9c3f5a2a0746da
fannypc11 <maria.porras@wizeline.com>
2022-01-17 16:18:32 -0600

    changing conventions

--- error_test.rb
+++ error_test.rb
@@ -1,47 +1,47 @@
-require_relative 'environment.rb'
+require_relative 'environment'
 
+# Class to test errors
 class ErrorTest
-    include Environment
-    attr_accessor :board, :depot, :control, :dictionary, :poker
-  
-    def initialize
-      @board = { players: Hash.new, control: Hash.new, status: nil }
-      @control = Environment::Control.new
-    end
-  
-    def new_worker name: nil
-      begin
-        @board[:players].store name, Environment::Worker.new(name)
-      rescue NoMethodError
-        puts 'No name for worker given'
-      end
-    end
+  include Environment
+  attr_accessor :board, :depot, :control, :dictionary, :poker
 
-    def set_worker_data name, type, data
-        begin
-            @board[:players][name].send("set_#{type}_data", data)
-        rescue NoMethodError
-            puts 'Worker or type of data not found'
-        end
-    end
-    
-    def estimate_payment_extra_hours worker, hours
-      begin
-        base_payment = @board[:players][worker].standard_shift[:payment]
-        total = base_payment * hours
-      rescue TypeError
-        puts 'Hours must be of type Integer'
-      end
+  def initialize
+    @board = { players: {}, control: {}, status: nil }
+    @control = Environment::Control.new
+  end
+
+  def new_worker(name: nil)
+    begin
+      @board[:players].store name, Environment::Worker.new(name)
+    rescue NoMethodError
+      puts 'No name for worker given'
     end
   end
-  
+
+  def set_worker_data(name, type, data)
+    begin
+      @board[:players][name].send("set_#{type}_data", data)
+    rescue NoMethodError
+      puts 'Worker or type of data not found'
+    end
+  end
+
+  def estimate_payment_extra_hours(worker, hours)
+    begin
+      base_payment = @board[:players][worker].standard_shift[:payment]
+      base_payment * hours
+    rescue TypeError
+      puts 'Hours must be of type Integer'
+    end
+  end
+end
 
 e = ErrorTest.new
 test = e.new_worker
 p test
 ana = e.new_worker(name: :Ana)
 p ana
-e.set_worker_data :ana, "personal", {surname: 'Porras', age: 27}
+e.set_worker_data :ana, 'personal', {surname: 'Porras', age: 27}
 p ana
 ana.initialize_shifts
 p ana
@@ -50,4 +50,4 @@ begin
 rescue ArgumentError
   puts 'Wrong number of arguments'
 end
-e.estimate_payment_extra_hours :Ana, "9"
+e.estimate_payment_extra_hours :Ana, '9'
