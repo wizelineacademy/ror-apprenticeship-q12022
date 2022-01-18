3e308db5d1a73ebe8dbf5e31895cba72d3069315
Erick Godoy <erick.godoy@wizeline.com>
2022-01-18 12:57:53 -0700

    Add Error handler to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,26 @@
 
 require 'resolv-replace' # This solve net issue 'Net::OpenTimeout: Failed to open TCP connection'
 
-pokedex_json = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=150')
-
-pokedex_json['results'].each do |item|
-  poke_request = HTTParty.get(item['url'])
-  poke_new = Pokemon.new
-  poke_new.name = poke_request['name']
-  poke_new.name[0] = poke_new.name[0].capitalize
-  poke_new.poke_id = poke_request['id']
-  poke_new.image_url = poke_request['sprites']['front_default']
-  poke_new.sort = poke_request['types'].map do |type|
-    type['type']['name'].slice(0, 1).capitalize + type['type']['name'].slice(1..-1)
-  end.join(',')
-  poke_new.save!
+begin
+  pokedex_json = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=15')
+  pokedex_json['results'].each do |item|
+    begin
+    poke_request = HTTParty.get(item['url'])
+    poke_new = Pokemon.new
+    poke_new.name = poke_request['name']
+    poke_new.name[0] = poke_new.name[0].capitalize
+    poke_new.poke_id = poke_request['id']
+    poke_new.image_url = poke_request['sprites']['front_default']
+    poke_new.sort = poke_request['types'].map do |type|
+      type['type']['name'].slice(0, 1).capitalize + type['type']['name'].slice(1..-1)
+    end.join(',')
+    poke_new.save!
+    rescue ActiveRecord::RecordNotUnique => e
+      Rails.logger.debug(e)
+    rescue StandardError => e
+      Rails.logger.debug(e)
+    end
+  end
+rescue StandardError => e
+  Rails.logger.debug(e)
 end
